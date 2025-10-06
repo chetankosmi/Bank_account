f7143277ed0336d4c07ceb1c3c978e1e2b0df33f
chetankosmi <chetankosmi24@gmail.com>
2025-10-06 17:17:19 +0530

    save to new Branch

--- Bank.py
+++ Bank.py
@@ -1,4 +1,11 @@
-initial_balance = float(input("Enter your initial balance: "))
-deposit_amount = float(input("Enter the deposit amount: "))
-updated_balance = initial_balance + deposit_amount
-print("\nUpdated Balance: ₹", updated_balance)
+initial_balance = float(input("Enter your initial balance: ₹"))
+deposit_amount = float(input("Enter the deposit amount: ₹"))
+balance = initial_balance + deposit_amount
+print("Balance after deposit: ₹", balance)
+withdraw_amount = float(input("Enter the amount to withdraw: ₹"))
+if withdraw_amount > balance:
+    print("Insufficient funds! Withdrawal denied.")
+else:
+    balance -= withdraw_amount
+    print("Withdrawal successful.")
+print("Final Balance: ₹", balance)
