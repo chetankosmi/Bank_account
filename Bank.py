0abfd819d9a1abe2ed2596580674487a9c341f5c
chetankosmi <chetankosmi24@gmail.com>
2025-10-06 17:12:33 +0530

    save

---
+++ Bank.py
@@ -0,0 +1,4 @@
+initial_balance = float(input("Enter your initial balance: "))
+deposit_amount = float(input("Enter the deposit amount: "))
+updated_balance = initial_balance + deposit_amount
+print("\nUpdated Balance: ₹", updated_balance)
